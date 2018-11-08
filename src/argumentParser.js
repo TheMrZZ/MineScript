9f5dd6535c593d16ad87427a416530277ef57f47
Florian ERNST <florianernst59@gmail.com>
2018-11-08 18:40:46 -0500

    Rename the "debug" mode into "developer"

--- src/argumentParser.js
+++ src/argumentParser.js
@@ -10,9 +10,9 @@ program
         'The resulting .mcfunction files are stored inside the given namespaceFolder - located inside a datapack.')
     .action((input, namespace) => { inputFolder = input; outputFolder = namespace; })
     .option('-w, --warnings', 'Activates the warnings.')
-    .option('-d, --debug', 'Activate the debug mode. Only useful for Minescript developers - not for users.')
     .option('-q, --quiet', "Don't show log messages. Errors will still be displayed.")
     .option('--no-footer', 'Hide the "Created with MineScript" comment at the end of functions.')
+    .option('--developer', 'Activate the developer mode. Only useful for Minescript developers - probably not for you.')
     .parse(process.argv)
 
 if (!process.argv.slice(2).length) {
