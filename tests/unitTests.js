cb772ed5e36a55161cbb8dafc058373c44e1e3d4
Florian ERNST <florianernst59@gmail.com>
2018-11-09 11:37:05 -0500

    Added unit tests for While, If, If Else, If Elif Else

--- tests/unitTests.js
+++ tests/unitTests.js
@@ -43,6 +43,36 @@ describe('script-parser', () => {
         it('should ignore whitespaces around }}', function () {
             assert.doesNotThrow(parse.bind(null, 'a=0\nsay {{ a  \n\t \n \t}}\nsay {{ a}}'), SyntaxError)
         })
+
+        it('should accept while', function () {
+            assert.doesNotThrow(parse.bind(null, '{%while false%}{%endwhile%}'), SyntaxError)
+            assert.doesNotThrow(parse.bind(null, '{%while false%}\n\t\n{%endwhile%}'), SyntaxError)
+        })
+
+        it('should accept if', function () {
+            assert.doesNotThrow(parse.bind(null, '{%if true%}{%endif%}'), SyntaxError)
+            assert.doesNotThrow(parse.bind(null, '{%if true%}\n\t\n{%endif%}'), SyntaxError)
+        })
+
+        it('should accept if-else', function () {
+            assert.doesNotThrow(parse.bind(null, '{%if false%}{%else%}{%endif%}'), SyntaxError)
+            assert.doesNotThrow(parse.bind(null, '{%if false%}\n{%else%}\n{%endif%}'), SyntaxError)
+        })
+
+        it('should accept if-elif-else', function () {
+            assert.doesNotThrow(parse.bind(null, '{%if false%}{%elif true%}{%else%}{%endif%}'), SyntaxError)
+            assert.doesNotThrow(parse.bind(null, '{%if false%}\n{%elif true%}\n{%else%}\n{%endif%}'), SyntaxError)
+        })
+
+        it('should not accept wrong end statements: while endif, if endwhile', function () {
+            assert.throws(parse.bind(null, '{%while true%}{%endif%}'), SyntaxError)
+            assert.throws(parse.bind(null, '{%if true%}{%endwhile%}'), SyntaxError)
+        })
+
+        it('should not accept wrong intermediate statements: while elif, while else', function () {
+            assert.throws(parse.bind(null, '{%while true%}{%else%}{%endwhile%}'), SyntaxError)
+            assert.throws(parse.bind(null, '{%while true%}{%elif false%}{%endwhile%}'), SyntaxError)
+        })
     })
 
     describe('parse', () => {
@@ -89,6 +119,35 @@ describe('script-parser', () => {
 
         it('should not unescape characters in String.raw', function () {
             assert.equal(parse('arg=String.raw`"hi\\nPlayer"`\nsay {{arg}}'), 'say "hi\\nPlayer"')
+        })
+
+        it('should correctly parse while statements', function () {
+            assert.equal(parse('i=0\n{% while i < 3 %}\ni += 1\nsay {{i}}\n{% endwhile %}'), 'say 1\nsay 2\nsay 3')
+        })
+
+        it('should correctly parse if statements', function () {
+            assert.equal(parse('{% if 7 > 3 %}\nsay Correct!\n{%endif%}'), 'say Correct!')
+            assert.equal(parse('{% if 3 > 7 %}\nsay Correct!\n{%endif%}'), '')
+        })
+
+        it('should correctly parse if-else statements', function () {
+            assert.equal(parse('{% if 7 > 3 %}\nsay Greater\n{%else%}\nsay Lower\n{%endif%}'), 'say Greater')
+            assert.equal(parse('{% if 3 > 7 %}\nsay Greater\n{%else%}\nsay Lower\n{%endif%}'), 'say Lower')
+        })
+
+        it('should correctly parse if-elif-else statements', function () {
+            assert.equal(
+                parse('{% if 3 > 3 %}\nsay Greater\n{%elif 3==3%}\nsay Equal\n{%else%}\nsay Lower\n{%endif%}'),
+                'say Equal'
+            )
+            assert.equal(
+                parse('{% if 7 > 3 %}\nsay Greater\n{%elif 7==3%}\nsay Equal\n{%else%}\nsay Lower\n{%endif%}'),
+                'say Greater'
+            )
+            assert.equal(
+                parse('{% if 3 > 7 %}\nsay Greater\n{%elif 3==7%}\nsay Equal\n{%else%}\nsay Lower\n{%endif%}'),
+                'say Lower'
+            )
         })
     })
 })
